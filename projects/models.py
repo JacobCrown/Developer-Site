3e26139f0d584eef7b13839138d2630df9f62a9a
Jacob Crown <jkorona.pl@gmail.com>
2022-02-02 11:52:12 +0100

    add Review model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 import uuid
 
@@ -17,3 +18,23 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Reviev(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up vote'),
+        ('down', 'Down vote'),
+    )
+    # owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True) 
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    id = models.UUIDField(
+        default=uuid.uuid4, # encoding type
+        unique=True,
+        primary_key=True, # use this field as primary key
+        editable=False, # cannot modify this in forms
+        )
+
+    def __str__(self):
+        return self.value
