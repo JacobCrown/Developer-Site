7ff89f19713ee5f6e606cf8b28e4bb58c3243705
Jacob Crown <jkorona.pl@gmail.com>
2022-02-02 11:57:52 +0100

    add Tag model make relations between models

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,9 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True) 
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, # encoding type
@@ -29,6 +32,7 @@ class Reviev(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True) 
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, # encoding type
         unique=True,
@@ -38,3 +42,20 @@ class Reviev(models.Model):
 
     def __str__(self):
         return self.value
+
+        
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, # encoding type
+        unique=True,
+        primary_key=True, # use this field as primary key
+        editable=False, # cannot modify this in forms
+        )
+    
+    def __str__(self):
+        return self.name
+
+        
+     
