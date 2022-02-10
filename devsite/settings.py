d75a61fb4eca0f27f8f65112d1c58c39903256c2
Jacob Crown <jkorona.pl@gmail.com>
2022-02-10 12:34:50 +0100

    add welcome email functionality

--- devsite/settings.py
+++ devsite/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from environs import Env
 
+
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +125,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Email config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'devsite.django@gmail.com'
+EMAIL_HOST_PASSWORD = env.str("EMAIL_PASSWORD")
 
 
 # Static files (CSS, JavaScript, Images)
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Profile
 
@@ -15,6 +17,18 @@ def profile_create(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
+
+        subject = 'Welcom to DevSite!'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
+        )
+    
 
 def update_user(sender, instance, created, **kwargs):
     profile = instance
